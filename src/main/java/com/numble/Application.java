6de7e338f1217b213a26a2e94ce8ec1fd0018395
jaesungahn91 <jaesungahn91@gmail.com>
2023-05-19 19:15:39 +0900

    init

---
+++ src/main/java/com/numble/Application.java
@@ -0,0 +1,13 @@
+package com.numble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularVoteArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopularVoteArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/numble/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularVoteArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
