b152efcb5cd4384836546a7872700c48b914e2d3
jaesungahn91 <jaesungahn91@gmail.com>
2023-05-28 23:43:16 +0900

    feat: 핸들러 누락 추가

--- command/src/main/java/com/numble/command/domain/PuppyEventHandler.java
+++ command/src/main/java/com/numble/command/domain/PuppyEventHandler.java
@@ -2,6 +2,8 @@ package com.numble.command.domain;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.numble.core.domain.PuppyCreatedEvent;
+import com.numble.core.domain.VoteCreatedEvent;
+import com.numble.core.domain.VoteDeletedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -13,8 +15,19 @@ public class PuppyEventHandler {
     private final PuppyEventSourcing puppyEventSourcing;
 
     @TransactionalEventListener
-    public void createEventHandle(PuppyCreatedEvent event) throws JsonProcessingException {
+    public void createPuppyEventHandle(PuppyCreatedEvent event) throws JsonProcessingException {
         puppyEventSourcing.send(event);
     }
+
+    @TransactionalEventListener
+    public void createVoteEventHandle(VoteCreatedEvent event) throws JsonProcessingException {
+        puppyEventSourcing.send(event);
+    }
+
+    @TransactionalEventListener
+    public void deleteVoteEventHandle(VoteDeletedEvent event) throws JsonProcessingException {
+        puppyEventSourcing.send(event);
+    }
+
 
 }
